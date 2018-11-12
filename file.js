d89bee95bda5e13a7266c10e9e49426f32275e37
1asalinas <42749574+1asalinas@users.noreply.github.com>
2018-11-12 13:56:02 -0600

    Add files via upload

---
+++ file.js
@@ -0,0 +1,27 @@
+//type your code inside this function
+function TorrentFile(){
+
+//first function
+function numberOfPacket(fileSize){
+    let packets = fileSize * 20;
+    return packets;
+
+}
+
+
+
+//second function
+function buildTorrentPacketArray(numOfPackets){
+    let list = [];
+    let length;
+    for(let i = 0 ; i < length; i++){
+        
+    }
+
+}
+
+
+
+}
+
+//main test methods
