8456912cf547c1b1e63e96c323ac29306320c23f
1asalinas <42749574+1asalinas@users.noreply.github.com>
2018-11-27 13:58:42 -0600

    start user

--- tracker.js
+++ tracker.js
@@ -4,11 +4,11 @@ function Tracker(){
   let seeds =[];
   let percent = [];
 
-  //implement all six functions below
+  //implement all five functions below
   function numOfSeeds(list){
     let result = 0;
     for (let i = 0; i <= list.length; i++){
-      if(list[i]!== null){
+      if(list[i] !== null){
         result++
       }
 
@@ -21,7 +21,7 @@ function Tracker(){
 
   function removeSeed(seeds,name){
     for(let i = 0; i < seeds.length; i++){
-      if(seeds[i]=== name);
+      if(seeds[i] === name);
       return seeds = [null];
       
     }
@@ -30,20 +30,25 @@ function Tracker(){
 
   function reportHealth(list){
     let sum = 0;
-    for(let i =0; i < list.length; i++){
+    for(let i = 0; i < list.length; i++){
       sum = sum + seeds[i];
     }
     return sum / list.length
   }
 
   function update(){
-  for (i = 0; i < seeds.length; i++){
-    let r = Math.random()
-    if(percent[i]){
-
+  for (let i = 0; i < seeds.length; i++){
+    let r = Math.random();
+    if(r < 0.03){
+      seeds[i] = null;
+      percent[i] = 0;
     }
-    else if
+    else if(r > percent[i]){
+      percent[i] = r;
+      } 
+    }
   }
 
-  }
+
+
 }
